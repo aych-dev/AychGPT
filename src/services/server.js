328d870e5cb9ce8374c9917fc456c21d97048316
aych <aychdev1@gmail.com>
2023-12-28 16:36:27 -0500

    updated backend requests

--- src/services/server.js
+++ src/services/server.js
@@ -1,27 +1,25 @@
-const express = require("express");
-const cors = require("cors");
+import OpenAI from 'openai';
+import express from 'express';
+import cors from 'cors';
+import { config } from 'dotenv';
+config();
+
 const PORT = 8000;
 const app = express();
-const axios = require("axios");
-require("dotenv").config();
+const openai = new OpenAI();
 
 app.use(express.json());
 app.use(cors());
 
-const API_KEY = process.env.API_KEY;
-
-app.get("/completions", async (req, res) => {
-  try {
-    const res = await axios.post("https://api.openai.com/v1/chat/completions", {
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "How are you? " }],
-      max_tokens: 100,
-    });
-    const data = await res.json();
-    res.send(data);
-  } catch (e) {
-    console.error(e);
-  }
+app.get('/completions', async (req, res) => {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: 'user', content: 'Who is the most expensive NFL player?' },
+    ],
+    model: 'gpt-3.5-turbo',
+    max_tokens: 10,
+  });
+  res.json(completion.choices[0].message.content);
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
