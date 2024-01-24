2addcd893a7f807b82d320ea98106a144b205539
aych <aychdev1@gmail.com>
2024-01-24 16:16:12 -0500

    added repo icon

--- src/components/Credit.tsx
+++ src/components/Credit.tsx
@@ -1,7 +1,18 @@
+import { FaGithub } from 'react-icons/fa';
+
 const Credit = () => {
   return (
     <div className='flex items-center justify-center text-red-500 font-semibold'>
-      <h1>Created by Aychdev</h1>
+      <h1>Created by Aychdev </h1>
+      <div className='ml-2'>
+        <a
+          href='https://github.com/aych-dev/AychGPT'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          <FaGithub />
+        </a>
+      </div>
     </div>
   );
 };
